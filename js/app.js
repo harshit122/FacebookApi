c114fe536a26e21da912e919bd1c208775ea781f
Harshit kyal <harshitkyal@gmail.com>
2018-04-25 11:35:42 +0530

    final changes

--- js/app.js
+++ js/app.js
@@ -42,116 +42,123 @@
      let getAboutInfo = () => {
 
          let myFacebookToken = $("#token").val();
-        //localStorage.setItem("lastname", myFacebookToken);
-        //console.log(localStorage.getItem("lastname"))
-         //myFacebookToken
-         $.ajax('https://graph.facebook.com/me?fields=picture.width(250).height(250),id,friends,name,first_name,last_name,birthday,about,hometown,languages,gender,education,work,relationship_status,quotes,family,website,favorite_athletes,favorite_teams,email,cover&access_token=' + myFacebookToken, {
+            console.log(myFacebookToken)
+             if (myFacebookToken === null || myFacebookToken == " " || myFacebookToken === undefined || myFacebookToken.length <=0 ){
 
-             success:(response) => {
-                 console.log(response);
-                 //console.log(typeof(response));
-                 $("#myEmail").text(response.email);
-                 //console.log(response.email);
-                 $("#myProfileId").html('<a target="blank" href="https://facebook.com/' + response.id + '">https://facebook.com/' + response.id + '</a>');
-                 $("#myHomeTown").text(response.hometown.name);
-                 $(".myProfilePic").attr("src", "" + response.picture.data.url + "");
-                 $(".myCoverPic").attr("src", "" + response.cover.source + "");
-                 $("#myName").html(response.name);
-                 $("#Namee").html(response.name);
-                 $("#myBirthday").html(response.birthday);
-                 $("#myGender").html(response.gender);
-                 $("#myAbout").html(response.about);
-                 $("#myWebsite").html(response.website || "Not Available");
-                 $("#myRelation").html(response.relationship_status || "Not Available");
-                 $("#myName").show(500);
-                 $("#Welcome").hide(500);
-                 $("#howWorks").hide(500);
-                 //for language 
-                 let languages = response.languages;
-                 let myLanguage = $.map(languages,(index) => {
-                     return index.name;
-                 });
-                 $("#myLanguage").text(myLanguage);
+                alert("Facebook Token Can't be left blank");
+                console.log("Facebook Token Can't be left blank")}
+            else
+            //localStorage.setItem("lastname", myFacebookToken);
+            //console.log(localStorage.getItem("lastname"))
+             //myFacebookToken
+             $.ajax('https://graph.facebook.com/me?fields=picture.width(250).height(250),id,friends,name,first_name,last_name,birthday,about,hometown,languages,gender,education,work,relationship_status,quotes,family,website,favorite_athletes,favorite_teams,email,cover&access_token=' + myFacebookToken, {
 
-                 // for work
-                 let work = response.work;
-                 let myWork = $.map(work, (index) => {
-                     return index.employer.name;
-                 });
-                 $("#myWork").text(myWork);
+                 success:(response) => {
+                     console.log(response);
+                     //console.log(typeof(response));
+                     $("#myEmail").text(response.email);
+                     //console.log(response.email);
+                     $("#myProfileId").html('<a target="blank" href="https://facebook.com/' + response.id + '">https://facebook.com/' + response.id + '</a>');
+                     $("#myHomeTown").text(response.hometown.name);
+                     $(".myProfilePic").attr("src", "" + response.picture.data.url + "");
+                     $(".myCoverPic").attr("src", "" + response.cover.source + "");
+                     $("#myName").html(response.name);
+                     $("#Namee").html(response.name);
+                     $("#myBirthday").html(response.birthday);
+                     $("#myGender").html(response.gender);
+                     $("#myAbout").html(response.about);
+                     $("#myWebsite").html(response.website || "Not Available");
+                     $("#myRelation").html(response.relationship_status || "Not Available");
+                     $("#myName").show(500);
+                     $("#Welcome").hide(500);
+                     $("#howWorks").hide(500);
+                     //for language 
+                     let languages = response.languages;
+                     let myLanguage = $.map(languages,(index) => {
+                         return index.name;
+                     });
+                     $("#myLanguage").text(myLanguage);
 
-                 //for education
-                 let favoriteAthletes = response.favorite_athletes;
-                 let myfavoriteAthletes = $.map(favoriteAthletes, (index) => {
-                     return index.name;
-                 });
-                 $("#favoriteAthletes").text(myfavoriteAthletes);
+                     // for work
+                     let work = response.work;
+                     let myWork = $.map(work, (index) => {
+                         return index.employer.name;
+                     });
+                     $("#myWork").text(myWork);
 
-                 // for family
-                 let family = response.friends.data;
-                 let myFamily = $.map(family, (index) => {
-                     return index.name;
-                 });
+                     //for education
+                     let favoriteAthletes = response.favorite_athletes;
+                     let myfavoriteAthletes = $.map(favoriteAthletes, (index) => {
+                         return index.name;
+                     });
+                     $("#favoriteAthletes").text(myfavoriteAthletes);
 
-                 $("#friends").text(myfavoriteAthletes);
+                     // for family
+                     let family = response.friends.data;
+                     let myFamily = $.map(family, (index) => {
+                         return index.name;
+                     });
 
-                 // for familyfavorite_teams
-                 let favoriteTeams = response.favorite_teams;
-                 //console.log(favoriteTeams)
-                 let myfavoriteTeams = $.map(favoriteTeams, (index) => {
-                     return index.name;
-                 });
+                     $("#friends").text(myfavoriteAthletes);
 
-                 $("#favoriteTeams").text(myfavoriteTeams);
+                     // for familyfavorite_teams
+                     let favoriteTeams = response.favorite_teams;
+                     //console.log(favoriteTeams)
+                     let myfavoriteTeams = $.map(favoriteTeams, (index) => {
+                         return index.name;
+                     });
 
-
-                 $("#myFamily").text(myFamily);
-
-                 $("#mainPic").show(500);
-                 $(".basic").show("scale");
-                 $(".feed6").show(500);
-                 $(".work").show("scale");
-                 $(".feed1").show(500);
-                 $(".family").show("scale");
-                 $(".feed2").show(500);
-                 $(".about").show("scale");
-                 $(".feed3").show(500);
-                 $(".contact").show("scale");
-                 $(".feed4").show(500);
-                 $(".experience").show("scale");
-                 $(".feed5").show(500);
-                 $("#post").show(200);
-                 $("#basic").show(500);
-                 $("#About").hide(200);
-                 $(".form-group").hide(500);
-
-                 $("#sign_out").show(200);
-                 $("#postt").hide();
-                 $(".pp").hide();
-             }, //end of success
-
-             //error handling
-             error: (req, status, error) => {
-                //alert("error")
-                console.log('Error occured', status, error);
-                alert("Just Refresh Again yet not solved then There's Something Wrong With Your TOKEN. Either not inserted or it is expired.");
-
-             },
-             //Loader
-             timeout: 4000,
-             beforeSend: () =>  {
-                 // move();
-                 $('.preloader').show();
-             },
-             complete: () => {
-                 $('.preloader').hide();
-
-             }
-
-             //end argument list 
+                     $("#favoriteTeams").text(myfavoriteTeams);
 
 
-         }); // end ajax call 
+                     $("#myFamily").text(myFamily);
+
+                     $("#mainPic").show(500);
+                     $(".basic").show("scale");
+                     $(".feed6").show(500);
+                     $(".work").show("scale");
+                     $(".feed1").show(500);
+                     $(".family").show("scale");
+                     $(".feed2").show(500);
+                     $(".about").show("scale");
+                     $(".feed3").show(500);
+                     $(".contact").show("scale");
+                     $(".feed4").show(500);
+                     $(".experience").show("scale");
+                     $(".feed5").show(500);
+                     $("#post").show(200);
+                     $("#basic").show(500);
+                     $("#About").hide(200);
+                     $(".form-group").hide(500);
+
+                     $("#sign_out").show(200);
+                     $("#postt").hide();
+                     $(".pp").hide();
+                 }, //end of success
+
+                 //error handling
+                 error: (req, status, error) => {
+                    //alert("error")
+                    alert("Just Refresh Again yet not solved then there is a problem with your facebook token . It is not correct or got expired.");
+
+                    console.log('Error occured', status, error);
+                 },
+                 //Loader
+                 timeout: 4000,
+                 beforeSend: () =>  {
+                     // move();
+                     $('.preloader').show();
+                 },
+                 complete: () => {
+                     $('.preloader').hide();
+
+                 }
+
+                 //end argument list 
+
+
+             }); // end ajax call 
+
 
 
 
@@ -218,7 +225,7 @@
              //error handling
              error: (req, status, error) => {
                 console.log('Error occured', status, error);
-                alert("Just Refresh Again yet not solved then There's Something Wrong With Your TOKEN. Either not inserted or it is expired.");
+                alert("Just Refresh Again yet not solved then There's Something Wrong With Your TOKEN, may be it is expired.");
 
              },
              //Loader
